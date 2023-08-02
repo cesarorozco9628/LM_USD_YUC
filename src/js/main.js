70a6e5789fb3dc4793d9cbb6799ff730aed88478
cesarorozco9628 <paulocesarorozcoguerrero@hotmail.com>
2023-08-02 12:04:01 -0600

    FIRST COMMIT

---
+++ src/js/main.js
@@ -0,0 +1,64 @@
+const cipher = salt => {
+    const textToChars = text => text.split('').map(c => c.charCodeAt(0));
+    const byteHex = n => ("0" + Number(n).toString(16)).substr(-2);
+    const applySaltToChar = code => textToChars(salt).reduce((a,b) => a ^ b, code);
+
+    return text => text.split('')
+      .map(textToChars)
+      .map(applySaltToChar)
+      .map(byteHex)
+      .join('');
+}
+
+function hash_text(text){
+    const myPassword = cipher('morganamx')
+     return myPassword(text)
+}
+
+const put_params_bulding = (data) => {
+    let { username ,id, valueBulding, stateBulding, currency, nameBulding, subDom,urlBulding } = data;
+
+    let is_mobile = /Mobile/i.test(navigator.userAgent);
+    let div_ifrm = document.getElementById(id);
+    let params = `${username?`username=${username}`:''}${valueBulding?`&price=${valueBulding}`:''}${stateBulding?`&stateBulding=${stateBulding}`:''}${currency?`&currency=${currency}`:''}${nameBulding?`&nameBulding=${nameBulding}`:''}${subDom?`&subDom=${subDom}`:''}${urlBulding?`&urlBulding=${urlBulding}`:''}`;
+    params = hash_text(params);
+
+    if (/Mobile/i.test(navigator.userAgent)) {
+      div_ifrm.innerHTML += `
+        <iframe 
+              src="http://nova.morgana.mx/quote/first_step/${params.length>0 ? `?${params}`:''}"
+              style="width:100%; height:700px;"
+          >
+          </iframe>
+       `
+    } else {
+    
+      // El código se está ejecutando en un dispositivo de escritorio
+
+    div_ifrm.innerHTML += `
+     <iframe 
+            src="http://nova.morgana.mx/quote/first_step/${params.length>0 ? `?${params}`:''}"
+            style="width:100%; height:550px;"
+        >
+        </iframe>
+
+      `
+    
+    }
+}
+
+window.onload = function() {
+    const ifm_mrg_data = document.getElementById('id_space_sb').dataset;
+    const data = {
+        username:'',
+        id:'id_space_sb',
+        valueBulding:ifm_mrg_data.valueBulding ? ifm_mrg_data.valueBulding : '0',
+        stateBulding:ifm_mrg_data.stateBulding ? ifm_mrg_data.stateBulding : 'CDMX', 
+        currency:ifm_mrg_data.currency  ? ifm_mrg_data.currency : 'MXN',
+        nameBulding: ifm_mrg_data.nameBulding ? ifm_mrg_data.nameBulding : '',
+        subDom: ifm_mrg_data.subDom ? ifm_mrg_data.subDom : '',
+        urlBulding: ifm_mrg_data.urlBulding ? ifm_mrg_data.urlBulding:'',
+
+    }
+   put_params_bulding(data)
+  };
